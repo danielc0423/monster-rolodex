ce8fa8a2e2a723536d25c645519cfc01d61297c0
danielc0423 <49470313+danielc0423@users.noreply.github.com>
2020-06-03 11:01:27 +0800

    Updates

---
+++ precache-manifest.522fc3b7764aa5356122432a529e343f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7233e8e3b552654b94f2ae816a995e89",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8de270911055a4218f84",
+    "url": "/monsters-rolodex/static/css/main.5be17841.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de270911055a4218f84",
+    "url": "/monsters-rolodex/static/js/main.ec563eed.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
